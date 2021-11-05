1abafa8e5def57e1f13dabb0f621bc25e1b844ab
Thomas Quetier <mamangvomi@gmail.com>
2021-11-05 15:20:50 +0100

    prise en compte d'un apk via androguard analyze

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
+import androguard
 # This is a sample Python script.
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import androguard.misc
 
+def inputFile(apk):
+    a = androguard.misc.AnalyzeAPK(apk)[0]
+    print(a)
+    print("Nom de 1\'application : ",a.get_app_name())
+    print("Permissions : ",a.get_permissions())
+    print("Activité principale:\n ",a.get_main_activity())
+    for act in a.get_activities():
+        print("Activity:", act)
+        print("Declares intent filters:\n", a.get_intent_filters('activity', act))
+    for ser in a.get_services():
+        print("Service:", ser)
+        print("Declares intent filters:\n", a.get_intent_filters('service', ser))
+    for rec in a.get_receivers():
+        print("Activity:", rec)
+        print("Declares intent filters:\n", a.get_intent_filters('receiverfl', rec))
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +28,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    inputFile("../apk/app-debug.apk")
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
