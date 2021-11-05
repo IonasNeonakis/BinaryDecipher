dbef8407225d681754907383e95e97c351ad3933
Thomas Quetier <mamangvomi@gmail.com>
2021-11-05 17:42:50 +0100

    Fouille avec succès des instr et autre objet de androguard + pseudo code en commentaire du main

--- src/main.py
+++ src/main.py
@@ -5,12 +5,17 @@ import androguard
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import androguard.misc
 
+map_methods = {}
+map_instructions = {}
+
+
 def inputFile(apk):
-    a = androguard.misc.AnalyzeAPK(apk)[0]
-    print(a)
-    print("Nom de 1\'application : ",a.get_app_name())
-    print("Permissions : ",a.get_permissions())
-    print("Activité principale:\n ",a.get_main_activity())
+    analyzedAPK = androguard.misc.AnalyzeAPK(apk)
+    a = analyzedAPK[0]
+    print("Infos générales : \n")
+    print("Nom de 1\'application : ", a.get_app_name())
+    print("Permissions : ", a.get_permissions())
+    print("Activité principale:\n ", a.get_main_activity())
     for act in a.get_activities():
         print("Activity:", act)
         print("Declares intent filters:\n", a.get_intent_filters('activity', act))
@@ -20,6 +25,48 @@ def inputFile(apk):
     for rec in a.get_receivers():
         print("Activity:", rec)
         print("Declares intent filters:\n", a.get_intent_filters('receiverfl', rec))
+    print("\n", "=" * 20)
+    print("Récupération des classes DEX : \n")
+    d = analyzedAPK[1]
+    main = "L" + a.get_main_activity().replace(".", "/") + ";"
+    for classdef in d:
+        c = classdef.get_class(main)
+        if c:
+            break
+    print("\n", "=" * 20, "\n", c)
+    for methods in c.get_methods():
+        methods.show()
+        print(methods.get_information())
+        print(methods.get_length() * 2)
+        print("nb reg : ",
+              methods.get_information().get("registers")[1] + 1 + len(methods.get_information().get("params", [])))
+
+        for instr in list(methods.get_instructions()):
+            print("length ", instr.get_length())
+            print("literaux ", instr.get_literals())
+        #     print("output " ,instr.get_output())
+
+
+# def ŝeudo eijf:
+#     nosMethodes = []
+#     pour chaque methode de la classe:
+#         instructions = []
+#         nbreg = get le nombre de registre
+#         offset = 0
+#         pour chaque instructions:
+#             corps = instr.get_output()
+#             instructions.append((offset, corps))
+#             on incr offset += instr.get_length()
+#         m = Methode()
+#         m.setInstr(instructions)
+#         m.setnbRegister(nbreg)
+#         nosMethodes.append(m)
+#     for m in nosMethodes:
+#         strOutput, bool = m.evaluate()
+#         if !bool:
+#             strOutput > Crapport
+#     "fini succès" > Crapport
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -29,8 +76,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     inputFile("../apk/app-debug.apk")
-
-
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
